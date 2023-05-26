55c1777f4f765714d584128143329ecc32f51b6a
malbasini <malbasini@gmail.com>
2023-05-27 00:02:30 +0200

    Commit

--- src/it/corso/java/polimorfismo/Cane.java
+++ src/it/corso/java/polimorfismo/Cane.java
@@ -2,12 +2,14 @@ package it.corso.java.polimorfismo;
 
 public class Cane extends Mammifero {
     private String nome;
+    private String razza;
 
-    public Cane(String nome){
+    public Cane(String nome, String razza){
         this.nome = nome;
+        this.razza = razza;
     }
     //OVERRIDE DEL METODO toString() DELLA CLASSE OBJECT
     public String toString(){
-        return "Il cane si chiama " + this.nome;
+        return "Il mio cane si chiama " + this.nome + " ed è un " + this.razza;
     }
 }
