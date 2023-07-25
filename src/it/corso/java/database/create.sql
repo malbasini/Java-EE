6021911dfc82ab28ddc1cc62478153eeb5bc354e
malbasini <malbasini@gmail.com>
2023-07-25 05:37:27 +0200

    Commit

--- src/it/corso/java/database/create.sql
+++ src/it/corso/java/database/create.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE `CorsoJava`DEFAULT CHARACTER SET utf8mb4
 COLLATE utf8mb4_0900_ai_ci  DEFAULT ENCRYPTION='N';
+
+
 CREATE TABLE `CorsoJava`.`clienti` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
